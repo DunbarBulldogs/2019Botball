1734ee5fd6f0c410f18221dfee79c7dbda812681
DunbarBulldogs <47642243+DunbarBulldogs@users.noreply.github.com>
2019-02-20 13:07:35 -0600

    Adding Create JM main.c file
    adding initial file

---
+++ main (2).c
@@ -0,0 +1,65 @@
+#include <kipr/botball.h>
+
+int main()
+{
+    enable_servos();                  
+    printf("Hello World\n");
+    create_connect();
+    set_servo_position(0,0);
+    set_servo_position(2,2047);
+    ao();
+    create_drive_direct(200,200);
+    msleep(1200);
+    ao();
+    set_servo_position(0,1614);
+    msleep(1000);
+    ao();
+    set_servo_position(2,144);
+    ao();
+    create_drive_direct(0,0);
+    msleep(5000);
+    ao();
+    create_drive_direct(50,-50);
+    msleep(3500);
+    ao();
+    create_drive_direct(200,200);
+    msleep(2000);
+    ao();
+    create_drive_direct(0,0);
+    msleep(1000);
+    set_servo_position(2,2047);
+    msleep(1000);
+    ao();
+    create_drive_direct(-200,200);
+    msleep(400);
+    ao();
+    create_drive_direct(200,200);
+    set_servo_position(0,1346);
+    msleep(1700);
+    ao();
+    create_drive_direct(0,0);
+    msleep(1700);
+    ao();
+    set_servo_position(2,144);
+    ao();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    disable_servos();
+    create_stop();
+    create_disconnect();
+    return 0;
+}
+
